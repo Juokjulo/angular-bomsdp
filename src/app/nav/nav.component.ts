63ef3abdc30c968b7fd9493bb269c66c81f619f8
Juokjulo <elisabeth.mansel@web.de>
2022-08-19 16:45:07 +0200

    6 files modified, 4 added

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-nav-basic',
+  templateUrl: './nav-component.html'
+})
+export class NavComponent {
+  active = 1;
+}
